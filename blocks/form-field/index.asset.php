817681854b9230ba0654e078911e7bc07f5338e6
Remco Tolsma <info@remcotolsma.nl>
2026-03-23 14:50:27 +0100

    build(blocks): track compiled block assets and mark them as generated

---
+++ blocks/form-field/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'a764126aeb64112c2ecb');
---
+++ blocks/form/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '5f3496428b4d5d6f745b');
