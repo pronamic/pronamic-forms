b738be3153ff9fb4a9b9bfea533ec9160c830c53
Remco Tolsma <info@remcotolsma.nl>
2026-03-24 11:39:26 +0100

    Revert "build(blocks): track compiled block assets and mark them as generated"
    This reverts commit 817681854b9230ba0654e078911e7bc07f5338e6.

--- blocks/form-radio-option/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'a58b62ad4c8c1452f935');
--- blocks/form/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '5f3496428b4d5d6f745b');
